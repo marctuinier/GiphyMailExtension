f354c37f550d0526e903d9417bfcc75956876232
Marc Tuinier <marctuinier@hotmail.com>
2023-09-16 20:00:42 +0200

    Initial Commit

---
+++ GIFMailSix/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GIFMailSix
+//
+//  Created by Marc Tuinier on 9/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GIFMailSix/GIFMailSixApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIFMailSixApp.swift
+//  GIFMailSix
+//
+//  Created by Marc Tuinier on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GIFMailSixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
